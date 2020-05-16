5e506dcaa017a24f23f3d8593b545cd33e23dffa
Mathias Weidner <buecher@mamawe.net>
2020-05-16 18:42:53 +0200

    approve that IKE header is valid

--- src/ipsec.c
+++ src/ipsec.c
@@ -24,14 +24,48 @@
 
 typedef struct __attribute__((__packed__)) {
 	uint64_t ispi, rspi;
-	uint8_t npl, version, extype, flags;
+	uint8_t npl;
+	unsigned int min_ver : 4, maj_ver : 4;
+	uint8_t extype, flags;
 	uint32_t mid, length;
 } ike_header;
 
+typedef struct __attribute__((__packed__)) {
+	uint8_t npl;
+	unsigned int reserved : 7;
+	unsigned int critical : 1;
+	uint16_t pl_length; 
+} ike_gph;	// generic paylod header
+
+#define MIN_IKE_DATAGRAM_LENGTH sizeof(ike_header)
+
+#define EXCHANGE_IKE_SA_INIT 34
+#define EXCHANGE_IKE_AUTH 35
+#define EXCHANGE_CREATE_CHILD_SA 36
+#define EXCHANGE_INFORMATIONAL 37
+
+#define NPL_NONE 0
+#define NPL_SA 33
+#define NPL_D 42
+#define NPL_V 43
+#define NPL_SK 46
+
+/**
+ * Approve that the IKE header is valid
+ *
+ * @param buf points at the beginning of the IKE header in the
+ *            datagramm.
+ *
+ *            This is not necessary the beginning of the UDP-Payload
+ *            since a NAT-T IKE datagramm starts with a non-ESP marker
+ *            that must be skipped when calling this function.
+ *
+ * @param buflen number of received octetts after buf
+ */
 int approve_ike_header(unsigned char *buf,
-		       ssize_t buflen,
-		       datagram_spec *ds) {
+		       ssize_t buflen) {
 	ike_header *ih = (ike_header *)buf;
+	uint32_t ih_length = ntohl(ih->length);
 	zlog_category_t *zc = zlog_get_category("IKE");
 	if (buflen < sizeof(ike_header)) {
 		zlog_debug(zc,
@@ -39,12 +73,59 @@ int approve_ike_header(unsigned char *buf,
 			   buflen);
 		return 0;
 	}
-	if (buflen < ih->length) {
+	if (buflen != ih_length) {
 		zlog_debug(zc,
-			   "datagram length (%ld) < length in IKE header (%ld)",
+			   "datagram length (%ld) doesn't match length in IKE header (%ld)",
 			   buflen,
-			   (long)ih->length);
+			   (long)ih_length);
 		return 0;
+	}
+	if (2 != ih->maj_ver || 0 != ih->min_ver) {
+		zlog_debug(zc,
+			   "unknown IKE version: %d.%d",
+			   ih->maj_ver,
+			   ih->min_ver);
+		return 0;
+	}
+	else {
+		zlog_info(zc,
+			   "IKE version: %d.%d",
+			   ih->maj_ver,
+			   ih->min_ver);
+	}
+	switch (ih->extype) {
+		case EXCHANGE_IKE_SA_INIT:
+		case EXCHANGE_IKE_AUTH:
+		case EXCHANGE_CREATE_CHILD_SA:
+		case EXCHANGE_INFORMATIONAL:
+			zlog_debug(zc,
+				   "exchange type: %hu",
+				   ih->extype);
+			break;
+		default:
+			zlog_debug(zc,
+				   "unknown exchange type: %hu",
+				   ih->extype);
+			return 0;
+	}
+	switch (ih->npl) {
+		case NPL_NONE:
+			zlog_debug(zc,
+				   "no next payload");
+			break;
+		case NPL_SA:
+		case NPL_D:
+		case NPL_V:
+		case NPL_SK:
+			zlog_debug(zc,
+				   "next payload: %hu",
+				   ih->npl);
+			break;
+		default:
+			zlog_debug(zc,
+				   "unknown next payload: %hu",
+				   ih->npl);
+			return 0;
 	}
 	zlog_info(zc,"IKE header OK");
 	return 1;
@@ -72,8 +153,7 @@ void ipsec_handle_datagram(int fd, ipsec_s * is) {
 			zlog_category_t *zc = zlog_get_category("IKE");
 			zlog_info(zc, "investigating IKE datagram");
 			if (!approve_ike_header(sm.buf,
-						result,
-						&ds)) {
+						result)) {
 				zlog_info(zc, "IKE datagram not approved");
 			}
 		}
@@ -86,8 +166,7 @@ void ipsec_handle_datagram(int fd, ipsec_s * is) {
 				zlog_category_t *zc = zlog_get_category("IKE");
 				zlog_info(zc, "investigating NAT-T IKE datagram");
 				if (!approve_ike_header(sm.buf+4,
-							result-4,
-							&ds)) {
+							result-4)) {
 					zlog_info(zc, "IKE datagram not approved");
 				}
 			}
